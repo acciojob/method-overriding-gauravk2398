634f43b034c962180f29faf7a2663d090ed48915
akashsInterra <109330154+akashsInterra@users.noreply.github.com>
2023-01-13 14:27:57 +0530

    first sub

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        A method = new B();
+        method.meth();
+        method.meth();
+
+    }
+  }
+class A{
+    String meth(){
+        return "right output";
+    }
+}
+class B extends A{
+    @Override
+    String meth() {
+        return "Method is overridden in Extendend class B";
+    }
 }
